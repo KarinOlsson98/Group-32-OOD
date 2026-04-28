19d6b9ec5cdb26ce73fd552bd946bd16c438be2d
Anton Bjäreborn <anton.bjareborn@gmail.com>
2026-04-28 11:37:17 +0200

    La till en ArrayList och metoden getMaterial

--- app/src/main/java/se/group32/application/MaterialManager.java
+++ app/src/main/java/se/group32/application/MaterialManager.java
@@ -1,5 +1,11 @@
 package se.group32.application;
+import se.group32.domain.Material;
+import java.util.ArrayList; 
 
 public class MaterialManager {
+    ArrayList<Material> materials = new ArrayList<Material>();
 
+    public ArrayList<Material> getMaterial(){
+        return materials;
+    }
 }
