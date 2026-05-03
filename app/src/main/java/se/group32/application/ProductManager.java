e9f00e4c6e3271e6a356899729cfbf40301b1e52
[Johanna Svensson] <[johannasvensson97@outlook.com]>
2026-05-03 17:14:59 +0200

    Lagt till metod för att skapa produkt i productmanager

--- app/src/main/java/se/group32/application/ProductManager.java
+++ app/src/main/java/se/group32/application/ProductManager.java
@@ -1,5 +1,24 @@
 package se.group32.application;
 
-public class ProductManager {
+import java.util.ArrayList;
+import java.util.List;
 
+import se.group32.domain.Material;
+import se.group32.domain.Product;
+
+public class ProductManager {
+    private List<Product> products = new ArrayList<>();
+    private int nextId = 1;
+
+    public void addProduct(String name, int lifespan,List<Material> materials) {
+        Product newProduct = new Product(nextId, name, lifespan, materials);
+
+        products.add(newProduct);
+
+        System.out.println("Product saved with ID: " + nextId); //För testkörning!!
+
+        nextId++;
+
+    }
 }
+
