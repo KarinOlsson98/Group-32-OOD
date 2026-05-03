6e818563e76b7c29e090b978089d25f04b4fccf2
[Johanna Svensson] <[johannasvensson97@outlook.com]>
2026-05-03 16:55:38 +0200

    Skapat produktens konstruktor

--- app/src/main/java/se/group32/domain/Product.java
+++ app/src/main/java/se/group32/domain/Product.java
@@ -1,5 +1,18 @@
 package se.group32.domain;
 
+import java.util.List;
+
 public class Product {
+    private int id;
+    private String name;
+    private int lifespan;
+    private List<Material> materials;
+
+    public Product(int id, String name, int lifespan, List<Material> materials) {
+        this.id = id;
+        this.name = name;
+        this.lifespan = lifespan;
+        this.materials = materials;
+    }
 
 }
