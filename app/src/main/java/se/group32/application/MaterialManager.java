475665b23c4656cb28f37151ce344dfc61a787f1
[Johanna Svensson] <[johannasvensson97@outlook.com]>
2026-05-03 19:19:09 +0200

    Lagt till metod för att kunna hämta material via id

--- app/src/main/java/se/group32/application/MaterialManager.java
+++ app/src/main/java/se/group32/application/MaterialManager.java
@@ -3,9 +3,19 @@ import se.group32.domain.Material;
 import java.util.ArrayList; 
 
 public class MaterialManager {
+    
     ArrayList<Material> materials = new ArrayList<Material>();
 
     public ArrayList<Material> getMaterial(){
         return materials;
+    }
+
+    public Material getMaterialById (int id) {
+        for (Material m : materials) {
+            if (m.getId() == id) {
+                return m;
+            }
+        }
+        return null;
     }
 }
