fe5f666a9cf8ad867105045512a904b17fd1e623
Anton Bjäreborn <anton.bjareborn@gmail.com>
2026-05-04 11:04:20 +0200

    La till en konstruktor med namn och impactValue

--- app/src/main/java/se/group32/domain/Material.java
+++ app/src/main/java/se/group32/domain/Material.java
@@ -1,5 +1,12 @@
 package se.group32.domain;
 
 public class Material {
+    private String name;
+    private int impactValue; //Borde kanske vara en double
+    //private RecyclingCategory recyclingCategory;
 
+    public Material(String name, int impactValue){
+        this.name = name;
+        this.impactValue = impactValue;
+    }
 }
