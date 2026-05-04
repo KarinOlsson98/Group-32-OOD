68a47b278cf1afbe0b3f35e71503906288cf43df
KarinOlsson98 <karin.olsson0090@stud.hkr.se>
2026-05-04 14:57:45 +0200

    Lagt till funktionaliteten för listProduct

--- app/src/main/java/se/group32/application/ProductManager.java
+++ app/src/main/java/se/group32/application/ProductManager.java
@@ -1,5 +1,15 @@
 package se.group32.application;
 
+import se.group32.domain.Product;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductManager {
+
+    private List<Product> products = new ArrayList<>();
+
+    public List<Product> getProducts() {
+        return products;
+    }
 
 }
