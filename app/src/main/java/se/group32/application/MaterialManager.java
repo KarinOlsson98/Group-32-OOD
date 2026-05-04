ec5e565ad160d08d54d645d4c6d796cf30ce98dd
Anton Bjäreborn <anton.bjareborn@gmail.com>
2026-05-04 11:09:52 +0200

    Lagt till en funktion för att skapa test-material

--- app/src/main/java/se/group32/application/MaterialManager.java
+++ app/src/main/java/se/group32/application/MaterialManager.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 public class MaterialManager {
     ArrayList<Material> materials = new ArrayList<Material>();
 
+    //En funktion för att testa lägga in material, temp tills ny lösning uppstår
+    public void addTestMaterial(){
+        Material plastic = new Material("plastic", 2);
+        Material wood = new Material("wood", 1);
+        materials.add(plastic);
+        materials.add(wood);
+    }
+
     public ArrayList<Material> getMaterial(){
         return materials;
     }
