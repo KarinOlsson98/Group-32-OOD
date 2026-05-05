4aba696142fae86f4e4b8c4394d1c33f247ef8ae
Da Jing <da_jing.trinh0010@stud.hkr.se>
2026-05-05 10:59:39 +0200

    050526: Tog bort kommentarer i ProductManager.java

--- app/src/main/java/se/group32/application/ProductManager.java
+++ app/src/main/java/se/group32/application/ProductManager.java
@@ -3,9 +3,6 @@ package se.group32.application;
 import se.group32.domain.Product;
 import se.group32.domain.RecyclingGuidance;
 
-// import se.group32.domain.RecyclingGuidance;
-// import se.group32.domain.Product;
-
 public class ProductManager {
     Product product = new Product();
     RecyclingGuidance guidance = new RecyclingGuidance();
