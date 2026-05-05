7371f9cb8da46eef0db5d24ce2dfab5d212266b7
Da Jing Trinh <dajingtrinh@gmail.com>
2026-05-06 00:43:27 +0200

    060526: Lagt till metoden productName(int id). Ska returna den givna produkten baserad på produktens ID.

--- app/src/main/java/se/group32/application/ProductManager.java
+++ app/src/main/java/se/group32/application/ProductManager.java
@@ -14,17 +14,23 @@ public class ProductManager {
         Product newProduct = new Product(nextId, name, lifespan, materials);
 
         products.add(newProduct);
+        System.out.println("Product saved with ID: " + nextId); //För testkörning!!
 
-    private List<Product> products = new ArrayList<>();
+        nextId++;
+    }
 
     public List<Product> getProducts() {
         return products;
     }
 
-        System.out.println("Product saved with ID: " + nextId); //För testkörning!!
-
-        nextId++;
-
+    // Get the product's name by searching their corresponding ID.
+    public String productName(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product.getName();
+            }
+        }
+        throw new IllegalArgumentException("Product with ID: " + id + " is invalid.");
     }
 }
 
